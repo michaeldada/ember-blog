f6133cf68bf063f378ec072da47dc8997d821751
Michael Dada <mail.michaeldada@gmail.com>
2016-03-31 13:15:59 -0700

    add route for individual posts and move edit and update functions to that route

---
+++ app/routes/post.js
@@ -0,0 +1,24 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('post', params.post_id);
+  },
+  actions: {
+    delete(post) {
+      post.destroyRecord();
+      this.transitionTo('index');
+    },
+    editPostForm() {
+      this.set('editPostForm', true);
+    },
+    edit(post, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          post.set(key, params[key]);
+        }
+      });
+      post.save();
+    }
+  }
+});
